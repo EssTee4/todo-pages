8f66cbb2c84f3d57333afd4cd9007208fd520e28
esstee <72905183+EssTee4@users.noreply.github.com>
2025-11-27 15:13:01 +0545

    Create 002_init.sql

---
+++ migrations/migrations/002_init.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE,
+  password TEXT,
+  salt TEXT
+);
+
+DROP TABLE IF EXISTS todos;
+
+CREATE TABLE todos (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER,
+  task TEXT,
+  completed INTEGER DEFAULT 0,
+  status TEXT DEFAULT 'todo'
+);
